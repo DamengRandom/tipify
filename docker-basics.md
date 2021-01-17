2f39a6064a000d2855ba89329328eaa63429651d
Damon Wu <damonmaozewu@gmail.com>
2021-01-17 15:41:59 +1100

    continuously added docker notes

--- docker-basics.md
+++ docker-basics.md
@@ -50,17 +50,23 @@ docker volume create --name=localstore
 
 1. Image: can treated as a `CD` which contains `Windows XP OS` inside
 
+
 2. Container: `having image(s)` inside and also run the image(s), inside container we have `kernel` (middleware) to connect processes running on your PC with your PC hardware
+
 
 3. docker run `behind the scenes`: docker client (user PC) -> docker server (docker cloud) -> pull/download docker related images (based on commands user typed) -> create a docker container and run the downloaded image
 [Formula: docker run = docker create + docker start] 
 
+
 4. `docker start -a`: `-a` means print the container logs to the user terminal !!!!!
+
 
 5. `docker system prune`: `delete` all containers & images from your PC
 
+
 6. docker stop vs kill: 
 stop is only for `stopping` the container, and having a `10` seconds to do some clean up before container get stopped, kill means `shut down immediately` no time to do clean up
+
 
 7. Meaning of `docker exec -it <container-id> commands`:
 ```
@@ -74,27 +80,36 @@ command: execute the command
 eg: docker exec -it <redi-container-id> redis-cli
 ```
 
+
 8. `docker exec -it <redi-container-id> sh`: sh means shell, which allows developer to type shell commands inside docker container for debugging purposes (Type `Ctrl + D` to exit the shell terminal)
 [eg: docker run -it busybox sh]
+
 
 9. `FROM`: Normally pull an image from docker server, eg: alpine
    `RUN`: Run the shell commands to install some software into image, such as redis
    `CMD`: RUn the command inside new created container
 
+
 10. Sometimes, if we change the RUN command sequence, we might improve the docker installation process
+
 
 11. Give a docker container a `name` instead of using hash !!
     `docker build -t dockerdamon/redis:latest .`
     Then we can run:
     `docker run dockerdamon/redis:latest`
 
+
 12. `docker run -p 8080:8080 docker-container-name`: -p means port, and first 8080 means incoming request port number from url, and second 8080 means docker handled port number, 2 different things, can port number can be different, request port number 7000, docker port number 8080 also works [8080 is the port number from node index.js file] !!
+
 
 13. `COPY ./ ./`: first ./ means local project root folder, second ./ means docker container root folder
 
+
 14. WORKDIR /usr/app: set up working directory for your project which is running inside docker container 
 
+
 15. `docker run -it -p 6870:6869 dockerdamon:latest`: a better command example for running docker container locally
+
 
 16. A way to boost up your build for npm install process:
 
@@ -105,3 +120,73 @@ COPY ./ ./
 # it will be faster than first time !!!
 ```
 
+
+17. `docker-compose`: A tool for running multiple docker containers at same time !!
+
+- allow developers to input multiple commands based on different services (such as database services, api services and web app services and etc)
+- Inside docker-compose file, dash (-) refers to array !!!
+
+docker restart container: 4 options:
+  - 'no': not restart container
+  - always: always attempt to restart the container
+  - on-failure: only restart if container stops with an error code
+  - unless-stopped: always restart unless developer force to stop it
+
+Commands:
+```
+`docker-compose up --build`: rebuild the docker containers
+`docker-compose down`: stop the current docker-compose process (turn off the containers, let containers take some break)
+`docker-compose logs -f`: check docker containers process (log the container's information) [inside docker -f: means: looking for file (which file)]
+`docker-compose ps`: check how many containers docker-compose tool is running now (Must under the docker-compose.yml folder to run this command, otherwise won't work)
+```
+
+
+18. docker `volumes`: we setup a reference mapping from docker inside container to local machine, make docker container referring/pointing/mapping to local folders, such as `/src`, `/public` and etc.
+
+Example of using docker volumes to produce <strong>hot reloading effect</strong> for react app:
+
+```
+Create a docker config file: `Dockerfile.dev`
+
+Then remove local `node_modules` folder
+
+Then run command: `docker build -f Dockerfile.dev`
+
+Then run docker container: `docker run -p 3000:3000 -v /app/node_modules -v $(pwd):/app <container_id>`
+```
+
+And here is the docker-compose version:
+
+```docker
+version: '3'
+services:
+  web:
+    build:
+      context: .
+      dockerfile: Dockerfile.dev
+    ports:
+      - "3000:3000"
+    volumes:
+      - /app/node_modules
+      - .:/app
+```
+
+*** Check codebase `docker-flow` for more details
+
+
+19. docker with nginx:
+
+Example command:
+
+```
+FROM node:alpine as builder
+WORKDIR '/app'
+COPY package.json .
+RUN yarn
+COPY . .
+RUN npm run build
+
+FROM nginx
+COPY --from=builder /app/build /usr/share/nginx/html
+
+```
--- public/service-worker.js
+++ public/service-worker.js
@@ -21,14 +21,14 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "5158404903d5cb14ce1995dcdb84d5f8"
+    "revision": "d552e171187e240f47633a31db0e8150"
   },
   {
     "url": "assets/css/1.styles.521b6668.css",
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   },
   {
-    "url": "assets/css/styles.1d14187e.css",
+    "url": "assets/css/styles.081b7b6e.css",
     "revision": "564bb139f1f8cd379110c4e18ebe970a"
   },
   {
@@ -72,8 +72,8 @@ self.__precacheManifest = [
     "revision": "93c85ee6ce0cad41cef027b4efd32350"
   },
   {
-    "url": "assets/js/18.586eebed.js",
-    "revision": "d9668847f0d34b782267071a286b740f"
+    "url": "assets/js/18.ceb1a515.js",
+    "revision": "2f7a835d598cf204f3e26b8c29452ec5"
   },
   {
     "url": "assets/js/19.8f420fde.js",
@@ -140,8 +140,8 @@ self.__precacheManifest = [
     "revision": "33116ad0944326c92500c5b86313c546"
   },
   {
-    "url": "assets/js/4.8218d736.js",
-    "revision": "2ca3b5cf999982fee353e0ae25f8c177"
+    "url": "assets/js/4.7cdc8f8e.js",
+    "revision": "2624db9ac56e9eac1c7e69543205acf5"
   },
   {
     "url": "assets/js/5.0c328728.js",
@@ -164,132 +164,132 @@ self.__precacheManifest = [
     "revision": "fbdc974864e0d99a9b221ae64c69d228"
   },
   {
-    "url": "assets/js/app.1d14187e.js",
-    "revision": "cc3095372e73eda8b4ca6a88fa288f8b"
+    "url": "assets/js/app.081b7b6e.js",
+    "revision": "af7ef42c2b2c893486f92c95079c5daa"
   },
   {
     "url": "css-bem.html",
-    "revision": "5a35f45b4d86f79b99136b5002af2fda"
+    "revision": "d317be6a33559b247c4bc58c1e608de3"
   },
   {
     "url": "docker-basics.html",
-    "revision": "e7de8ae405711c3d495fed69d6b1f1cc"
+    "revision": "48f320c58491edeef15f0eba34a7198e"
   },
   {
     "url": "dotnet-basics.html",
-    "revision": "59addcfe3b709e51aec5a863af529f17"
+    "revision": "6d76a9d637c5a1b74b98121f0a33f53a"
   },
   {
     "url": "dotnet-cqrs.html",
-    "revision": "e83251cbf92f96bc7345248053098581"
+    "revision": "fe3c778bf0dbd62d850e456142ea9308"
   },
   {
     "url": "errors-I-saw.html",
-    "revision": "6966c32ed23e8454e16cae245a93e9e1"
+    "revision": "c8712de4de3a3a009b967329f8a4a880"
   },
   {
     "url": "git-tips-one.html",
-    "revision": "84f6ce7c5ead1ce307ae131475554b8d"
+    "revision": "31b31da5d53542c668de3d35b2c9a5c6"
   },
   {
     "url": "index.html",
-    "revision": "be994c0574fdf002ea997c46f91592e9"
+    "revision": "cb046964590014808fcd19b47d705c36"
   },
   {
     "url": "js-callback-promise-async-await.html",
-    "revision": "839d588b6330c750fdd9b03ba17de1e0"
+    "revision": "de2d1a1cdcffe4059ce69388a022a6f7"
   },
   {
     "url": "js-closure.html",
-    "revision": "285fef665abb9a77f8b28525c4ed7a34"
+    "revision": "fd639c467215893f8231bed3fab2e23e"
   },
   {
     "url": "js-event-loop.html",
-    "revision": "07da2efcda39b665e3eca0d8b21f6030"
+    "revision": "ee9aef6778e74026b893a7d1926179d6"
   },
   {
     "url": "js-general-tips.html",
-    "revision": "8e31c44d05475a7064f66826e4cddaba"
+    "revision": "2f034aa215dec1441b30c725c71ad891"
   },
   {
     "url": "js-high-order-functions.html",
-    "revision": "cf53e6c6d4e1952ef08295f26a1427b9"
+    "revision": "7be4498e4c64ec960eb51c868e747ec1"
   },
   {
     "url": "js-hoisting.html",
-    "revision": "244877a61f73721d7311b8d7af2c42e8"
+    "revision": "52ee87d0f5fcdb53336387b302d1a1c1"
   },
   {
     "url": "js-immutability.html",
-    "revision": "2e4f78721a19d2f3276347c4b71ac5cc"
+    "revision": "04bf3564a329cb58185cc1c5528cbbb3"
   },
   {
     "url": "js-module-systems.html",
-    "revision": "566c5fb116d58ef7119361b441446f99"
+    "revision": "760bfb6b9925e6fcb17acf069c78ef1b"
   },
   {
     "url": "js-reduce-method.html",
-    "revision": "2776718e5cc09af1a80ba54b5671ffe7"
+    "revision": "b8e037d854a99e01ef46300501abe570"
   },
   {
     "url": "one-word-tips.html",
-    "revision": "5fb3f43d4b04e6d798e2096e3f5c8eb8"
+    "revision": "44f7ae5390767e03d849094002af9839"
   },
   {
     "url": "postgres-tips.html",
-    "revision": "0687a65071cdfec28748d4997c776b75"
+    "revision": "c9be89e8deb4f5daf173a4109e2c411a"
   },
   {
     "url": "react-hoc.html",
-    "revision": "ed60342c3da91b1b5b54169b866fefe9"
+    "revision": "168f7d5751cc3ccd5ab9be4d47758455"
   },
   {
     "url": "react-hooks-tips.html",
-    "revision": "0fffe7f452b58d937493d563189a976e"
+    "revision": "fb26d660fda629da11fcd66ab2c77700"
   },
   {
     "url": "react-lifecycle.html",
-    "revision": "105f39f3b70ff8973939f38445ca6240"
+    "revision": "475f9f2208d7d0160ea0e5eced3eabd4"
   },
   {
     "url": "react-tips-one.html",
-    "revision": "e0b7e0a7962946007a77426f2562d525"
+    "revision": "27777a10368f35bb1e928f5839cec1ad"
   },
   {
     "url": "redux-tips.html",
-    "revision": "b6f86e272758e681dc96764509110408"
+    "revision": "e2e6d7d0dfbc3b835ffbd77cfa39d445"
   },
   {
     "url": "server-side-rendering.html",
-    "revision": "c7225d4fdfbb13065ef8ce93cbba2670"
+    "revision": "35f875b9ca34f3cc812271cf129cab94"
   },
   {
     "url": "sql-tips.html",
-    "revision": "adf96ee599ab42ef049092c79c8c6434"
+    "revision": "7ba74f9677914625c4462e21b07447d5"
   },
   {
     "url": "tipify.html",
-    "revision": "18639a359f90875e62a2ca234fe81072"
+    "revision": "45e11599aeb667d089b64d19164fe778"
   },
   {
     "url": "ts-basics.html",
-    "revision": "ae4e76a12e3a171941d6c0c90628b1c8"
+    "revision": "4e6d28280b7cd5d85d12748c20164793"
   },
   {
     "url": "vuejs-tips.html",
-    "revision": "9d2a13d6b3fe1532954df2b37acfb9f0"
+    "revision": "cdf0446cd597683fc959521b79b7e3aa"
   },
   {
     "url": "vuex-terms.html",
-    "revision": "42e29242df6fa40cafc031e1ad327e22"
+    "revision": "837c877424af277d6f7c8c3bd065a2ba"
   },
   {
     "url": "webpack-tips.html",
-    "revision": "0571838d3e78f8c2d47b7c29fad4393f"
+    "revision": "dd7371ea580dfa36110cf15108e10819"
   },
   {
     "url": "weekly-tips.html",
-    "revision": "867e4a550e4bc4e769c564e980cd61ee"
+    "revision": "2f93e24b57f197e22b2d8dcf36d20e21"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
